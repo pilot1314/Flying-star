b5765716bc5dd75f210f35fcee03a6ccdfcbcde2
86158 <nieyuhang1222@163.com>
2024-08-02 00:03:56 +0800

    第一次修改

---
+++ src/com/Nyh/AP/Apdmi.java
@@ -0,0 +1,13 @@
+package com.Nyh.AP;
+
+public class Apdmi{
+    public static void main(String[] args) {
+        int i = 0;
+        for (int j = 0; j < 101; j++) {
+            i=j++;
+            System.out.println(i+"\\"+j);
+        }
+
+
+    }
+}
