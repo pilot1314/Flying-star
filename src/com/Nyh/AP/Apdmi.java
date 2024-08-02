0dfcb2377d859c0659f650ccbc8dbb876837ea07
86158 <nieyuhang1222@163.com>
2024-08-02 17:49:59 +0800

    第一次改进的

--- src/com/Nyh/AP/Apdmi.java
+++ src/com/Nyh/AP/Apdmi.java
@@ -3,9 +3,9 @@ package com.Nyh.AP;
 public class Apdmi{
     public static void main(String[] args) {
         int i = 0;
-        for (int j = 0; j < 101; j++) {
-            i=j++;
-            System.out.println(i+"\\"+j);
+        for (int j = 1; j < 101; j++) {
+            i++;
+            System.out.println("第"+i+"个数= "+j);
         }
 
 
